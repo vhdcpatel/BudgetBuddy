a142557877dea77bca451d1082c0cdc294e81605
Vraj Patel <vrajhpatel21@gmail.com>
2023-04-16 12:39:30 +0530

    added filter future

--- src/components/Expense/DisplayExpense.js
+++ src/components/Expense/DisplayExpense.js
@@ -1,10 +1,22 @@
 import ExpenseItems from "./ExpenseItems";
-import "./DisplayExpense.css"
-import Card from "../UI/Card"
+import ExpenseFilter from "./FilterExpense";
+import "./DisplayExpense.css";
+import Card from "../UI/Card";
+import { useState } from "react";
 
 function DisplayExpense(props) {
+
+  const [filterYear,setFilterYear] = useState('2020')
+
+  const filterChangeHandler = (selectedYear) =>{
+    console.log(selectedYear);
+    setFilterYear((filterYear) => (filterYear = selectedYear));
+  }
+  console.log(filterYear);
+
   return (
     <Card className="expenses">
+      <ExpenseFilter onSelect={filterChangeHandler}></ExpenseFilter>
       {props.listItems.map((expense) => (
         <ExpenseItems
           title={expense.title}
